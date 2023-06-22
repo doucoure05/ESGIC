ea0a91f51e2e8f334888f748c6266024fc8d81be
Doucoure05 <doucbouba104@gmail.com>
2023-06-22 15:06:53 +0000

    diplome format is ok and qrcode is ok

--- diplome/src/components/GenererDiplome.js
+++ diplome/src/components/GenererDiplome.js
@@ -6,8 +6,9 @@ import Button from "react-bootstrap/esm/Button";
 import axios from 'axios';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bulma/css/bulma.css";
-import jsPDF from 'jspdf';
+// import jsPDF from 'jspdf';
 import QRCode from "qrcode.react";
+import html2pdf from 'html2pdf.js';
 
 function GenererDiplome() {
 
@@ -22,34 +23,45 @@ function GenererDiplome() {
     const [date_deliv, setDate_deliv] = useState("");
     const [qrcodeValue, setQrcodeValue] = useState("");
     const { id } = useParams();
-    
+
 
     useEffect(() => {
         getDiplomeById();
         console.log('id diplome: ' + id);
     }, []);
 
-    const generateDiplome = () => {
-        let doc = new jsPDF("l", "pt", "a4");
-        doc.html(document.querySelector("#pd"), {
-            callback: function (pdf) {
-                let pageCount = doc.internal.getNumberOfPages();
-                pdf.deletePage(pageCount);
-                pdf.save(nom + ".pdf");
-            }
-        });
-    };
+    // const generateDiplome = () => {
+    //     let doc = new jsPDF("l", "pt", "a4");
+    //     doc.html(document.querySelector("#pd"), {
+    //         callback: function (pdf) {
+    //             let pageCount = doc.internal.getNumberOfPages();
+    //             pdf.deletePage(pageCount);
+    //             pdf.save(nom + ".pdf");
+    //         }
+    //     });
+    // };
 
-    const generateDiplome2 = () => {
-        window.onload = function () {
-            let doc = new jsPDF("l", "pt", "a4");
-            doc.addHTML(document.querySelector("#pd"), function () {
-                doc.save( nom + ".pdf");
-            });
-        }
-    };
+    // const generateDiplome2 = () => {
+    //     window.onload = function () {
+    //         let doc = new jsPDF("l", "pt", "a4");
+    //         doc.addHTML(document.querySelector("#pd"), function () {
+    //             doc.save( nom + ".pdf");
+    //         });
+    //     }
+    // };
 
-
+    const generateDiplome3 = () => {
+        const element = document.getElementById('pageContent');
+        const options = {
+            filename: nom + '.pdf',
+            image: {
+                type: 'jpeg', quality: 0.98
+            },
+            html2canvas: { scale: 1 },
+            jsPDF: { unit: 'pt', format: 'a4', orientation: 'landscape' }
+        };
+        html2pdf().from(element).set(options).save();
+    }
 
     const getDiplomeById = async () => {
         const response = await axios.get(`http://localhost:5000/diplome/${id}`);
@@ -68,16 +80,30 @@ function GenererDiplome() {
 
     return (
         <>
-            <div>
-                <h2 className="text-center display-4">
-                    Diplome
-                </h2>
+            <div className="row">
+                <div className="col-sm-7">
+                    <h2 className="text-right display-4">
+                        Diplome
+                    </h2>
+                </div>
+                <div className="col-sm-3">
+                    <br />
+                    <Button style={{ textAlign: "right" }}
+                        className="Button"
+                        variant="success"
+                        onClick={generateDiplome3}
+                    >telecharger
+                        <i className="bi bi-file-text"></i>
+                    </Button>
+                </div>
+                <div className="col-sm-2"></div>
             </div>
-            <div id="pd" className="div">
+            <br />
+
+            <div id="pageContent" className="div">
                 <div className="row">
                     <div className="col-sm-2">
                         <img id='image' src="/images/esgic.png" alt="Diplome Logo" />
-                        <QRCode className='h-50 w-50 pl-1 pr-0' value={qrcodeValue} />
                     </div>
                     <div className="col-sm-5">
                         <h5 style={{ color: "blue" }}>Ecole Superieur de Gestion, d'Informatique et de Comptabilité <br /></h5>
@@ -96,67 +122,57 @@ function GenererDiplome() {
                                 {/* azerty */}
                             </div>
                             <div className="col-sm-6">
+                                <br />
                                 <h6 style={{ textAlign: "center", background: 'gray', padding: 4 }}>N° {id}A/CP-ESGIC</h6>
                             </div>
                         </div>
                     </div>
                 </div>
-                <h3 id="h3">ATTESTATION DU DIPLOME</h3>
+                <div className='row'>
+                    <div className='col-sm-2'><QRCode className='h-50 w-75 pl-3' value={qrcodeValue} /></div>
+                    <div className='col-sm-10 pt-3'> <br /><h1 id="h3"> ATTESTATION DU DIPLOME</h1></div>
+                </div>
                 {/* <hr id='hr'></hr>  */}
-                <h6 style={{ textAlign: "center", paddingLeft: 5 + "px" }}>Nous soussignés, Ecome Superieur de Gestion, d'Informatique et Comptabilité(ESGIC ), attestons que <strong>Mr/Mlle/Mme</strong> </h6>
-                <h2 style={{ textAlign: "center" }}><strong>{nom} né(e) le {date_nais}</strong></h2>
+                <h5 style={{ textAlign: "center", paddingLeft: 5 + "px" }}>Nous soussignés, Ecome Superieur de Gestion, d'Informatique et Comptabilité(ESGIC ), attestons que <strong>Mr/Mlle/Mme</strong> </h5>
+                <h3 style={{ textAlign: "center" }}><strong>{nom} né(e) le {date_nais}</strong></h3>
                 <h4 style={{ textAlign: "center" }}><strong>à {lieu} - <u>N°Mle :</u> {matricule}/UT</strong></h4>
                 {dp === "DUT" ? (
-                    <h6 style={{ textAlign: "center" }}>à été déclaré(e) définitivement admis(e) à l'examin de fin de cycle du <br /> {dp}(Diplome Universitaire de Technologie) de l'ESGIC. </h6>
+                    <h5 style={{ textAlign: "center" }}>à été déclaré(e) définitivement admis(e) à l'examin de fin de cycle du <br /> {dp}(Diplome Universitaire de Technologie) de l'ESGIC. </h5>
                 ) : (
-                    <h6 style={{ textAlign: "center" }}>à été déclaré(e) définitivement admis(e) à l'examin de fin de cycle du <br /> {dp} de l'ESGIC. </h6>
+                    <h5 style={{ textAlign: "center" }}>à été déclaré(e) définitivement admis(e) à l'examin de fin de cycle du <br /> {dp} de l'ESGIC. </h5>
                 )}
                 <div className="row">
                     <div className="col-sm-6">
-                        <h6 style={{ paddingLeft: 20 + "px" }}><strong><u>Session de </u>: {session} - Centre de l'ESGIC</strong></h6>
+                        <h5 style={{ paddingLeft: 20 + "px" }}><strong><u>Session de </u>: {session} - Centre de l'ESGIC</strong></h5>
                     </div>
                     <div className="col-sm-6">
-                        <h6><strong><u>Filière </u>: {filiere}</strong></h6>
+                        <h5><strong><u>Filière </u>: {filiere}</strong></h5>
                     </div>
                 </div>
                 <h4 style={{ textAlign: "center", color: "red" }}><strong><u>Mention </u>: {mention}</strong></h4>
-                <h6 style={{ textAlign: "center" }}>En foi de quoi la présente attestation lui est délivré pour servir et valoir ce que de droit.</h6>
+                <h5 style={{ textAlign: "center" }}>En foi de quoi la présente attestation lui est délivré pour servir et valoir ce que de droit.</h5>
                 <h6 style={{ textAlign: "right", paddingRight: 20 + "px" }}>Bamako, le <strong>{date_deliv}</strong></h6>
                 <div className="row">
                     <div className="col-sm-4">
-                        <h6 style={{ paddingLeft: 20 + "px" }}><strong>Le Directeur des Etudes</strong></h6>
+                        <h5 style={{ textAlign: 'left', paddingLeft: 5 + "px" }}><strong>Le Directeur des Etudes</strong></h5>
                         <br />
                         <br />
-                        <h6 style={{ paddingLeft: 20 + "px" }}><strong>Aguibou OUATTARA</strong></h6>
+                        <h5 style={{ textAlign: 'left', paddingLeft: 5 + "px" }}><strong>Aguibou OUATTARA</strong></h5>
                     </div>
                     <div className="col-sm-5">
-                        <img style={{ width: 120 + "px", height: 50 + "px", marginLeft: 80 + "px" }} src="/golde_medail2_reduit.jpeg" alt="Médaille" />
-                        <h6 style={{textAlign: "left", fontSize: 10 + "px", marginTop: 0 + "px"}}><br /><br /><br /><strong><u>NB</u>: La présente attestation n'est délivrée q'une seule fois.</strong></h6>
+                        <img style={{ width: 120 + "px", height: 50 + "px", marginLeft: 110 + "px" }} src="/golde_medail2_reduit.jpeg" alt="Médaille" />
+                        <h4 style={{ textAlign: "center", fontSize: 14 + "px", marginTop: 0 + "px" }}><br /><br /><strong><u>NB</u>: La présente attestation n'est délivrée q'une seule fois.</strong></h4>
                     </div>
                     <div className="col-sm-3">
-                        <h6 style={{ textAlign: "right", paddingRight: 20 + "px" }}><strong>Le Directeur Général</strong></h6>
+                        <h5 style={{ textAlign: "right", paddingRight: 5 + "px" }}><strong>Le Directeur Général</strong></h5>
                         <br />
                         <br />
-                        <h6 style={{ textAlign: "right", paddingRight: 20 + "px" }}><strong>Diakaridia CAMARA</strong></h6>
+                        <h5 style={{ textAlign: "right", paddingRight: 5 + "px" }}><strong>Diakaridia CAMARA</strong></h5>
                     </div>
+                    
                 </div>
             </div>
-            <br/>
-            <div className="row">
-                <div className="col-sm-5"></div>
-                <div className="col-sm-2">
-                    <Button style={{ textAlign: "center" }}
-                        className="Button"
-                        variant="success"
-                        onClick={generateDiplome}
-                    >telecharger
-                        <i className="bi bi-file-text"></i>
-                    </Button>
-                </div>
-                <div className="col-sm-5"></div>
-            </div>
-            
-            <br />
+
         </>
     );
 }
