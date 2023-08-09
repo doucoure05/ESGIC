651705e64ffe736fb58bd0e85edaecbebb514501
Doucoure05 <doucbouba104@gmail.com>
2023-08-09 13:12:32 +0000

    updated server/index.js code

--- server/index.js
+++ server/index.js
@@ -11,10 +11,22 @@ app.use(cors({}));
 app.use(express.json());
 app.use(UserRoute);
 app.use(DiplomeRoute);
+const morgan = require('morgan');
+const cors = require('cors');
 
+const APP_PORT = 5000;
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 5000;
-}
-app.listen(port, () => console.log('Server up and running!!!, listening to port ' + port));
+app.use(morgan('combined'));
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.send('Hello from the back-end.');
+});
+
+app.listen(APP_PORT, () =>console.log('Webserver listening to port' + APP_PORT));
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 5000;
+// }
+// app.listen(port, () => console.log('Server up and running!!!, listening to port ' + port));
